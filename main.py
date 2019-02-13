558a58b9b99297fc890c8b1955107a11e484ded2
Jane Sun <jane.sun@supplylogx.io>
2019-02-13 13:54:00 -0600

    add vectors code

--- main.py
+++ main.py
@@ -5,3 +5,33 @@
 # that new vector
 # find the norm of the final vector
 # also find the dot product between the 2nd and 3rd vector
+import vector as v
+
+vecs = []
+with open('vectors.txt') as f:
+    for line in f:
+        # vx, vy = line.strip().split(',')
+        # vec = Vector(vx, vy)
+        # print(vx, vy)
+
+        # vec = v.Vector(line.strip().split(','))
+
+        vec = line.strip().split(',')
+        # vec = map(v.myfloat(vec), vec)
+        vec = list(map(lambda x: float(x), vec))
+        vec = v.Vector(*vec)
+        vecs.append(vec)
+
+# print(len(vecs))
+# print(vecs[1])
+# print(vecs)
+
+v1 = vecs[0].scale(3)
+print(v1)
+print(vecs)
+
+# vx = list[vecs[i][0] for i in len(vecs)]
+# print(vx)
+
+# num1 = [vecs.vx for vec in vecs]
+# print(num1)
--- vector.py
+++ vector.py
@@ -6,3 +6,32 @@
 # norm or len
 # dot product
 # override __str__ and __repr__
+
+
+class Vector:
+    def __init__(self, *argv):
+        self.argv = argv  # tuple
+
+    def __add__(self, x, y):
+        pass
+
+    def myfloat(self, n):
+        return float(n)
+
+    def scale(self, scaler):
+        return Vector(*[n * scaler for n in self.argv])
+
+    def __add__(self, other):
+        return Vector(*[c1 + c2 for c1, c2 in zip(self.nums, other.nums)])
+
+    def dot(self, other):
+        return sum((c1 * c2 for c1, c2 in zip(self.nums, other.nums)))
+
+    def norm(self):
+        return self.dot(self) ** 0.5
+
+    def __str__(self):
+        return "{}".format(self.argv)
+
+    def __repr__(self):
+        return self.__str__()
